1c1ec1b596391f468288eb64b8dc26dd2085a92f
itgo <634872896@qq.com>
2019-10-29 14:33:59 +0800

    refactor: beauty code

--- client.c
+++ client.c
@@ -9,48 +9,36 @@
 
 int main()
 {
-    int client_socket = socket(AF_INET, SOCK_STREAM, 0); //创建和服务器连接套接字
-    if (client_socket == -1)
-    {
-        perror("socket");
-        return -1;
-    }
-    struct sockaddr_in addr;
-    memset(&addr, 0, sizeof(addr));
+    int client_socket = socket(AF_INET, SOCK_STREAM, 0);
 
-    addr.sin_family = AF_INET;                /* Internet地址族 */
-    addr.sin_port = htons(PORT);              /* 端口号 */
-    addr.sin_addr.s_addr = htonl(INADDR_ANY); /* IP地址 */
+    struct sockaddr_in addr;
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(PORT);
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
     inet_aton("127.0.0.1", &(addr.sin_addr));
 
-    int addrlen = sizeof(addr);
-    int listen_socket = connect(client_socket, (struct sockaddr *)&addr, addrlen); //连接服务器
-    if (listen_socket == -1)
-    {
-        perror("connect");
-        return -1;
-    }
+    int server_socket = connect(client_socket, (struct sockaddr *)&addr, sizeof(addr)); //连接服务器
 
-    printf("成功连接到一个服务器\n");
+    printf("connect success\n");
 
     char buf[SIZE] = {0};
 
-    while (1) //向服务器发送数据，并接收服务器转换后的大写字母
+    while (1)
     {
-        printf("请输入你相输入的：");
+        printf("Send to server：");
         scanf("%s", buf);
         write(client_socket, buf, strlen(buf));
 
         int ret = read(client_socket, buf, strlen(buf));
 
-        printf("buf = %s", buf);
+        printf("From server = %s", buf);
         printf("\n");
-        if (strncmp(buf, "END", 3) == 0) //当输入END时客户端退出
+        if (strncmp(buf, "end", 3) == 0)
         {
             break;
         }
     }
-    close(listen_socket);
+    close(server_socket);
 
     return 0;
 }
--- server.c
+++ server.c
@@ -1,4 +1,4 @@
-#include <sys/types.h> /* See NOTES */
+#include <sys/types.h>
 #include <sys/socket.h>
 #include <stdio.h>
 #include <netdb.h>
@@ -6,21 +6,21 @@
 #include <unistd.h>
 #include <string.h>
 
-#define PORT 9990 //端口号
-#define SIZE 1024 //定义的数组大小
+#define PORT 9990
+#define SIZE 1024
 
 int creat_socket()
 {
-    int server_socket = socket(AF_INET, SOCK_STREAM, 0); //创建一个负责监听的套接字
+    int server_socket = socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in addr;
 
-    addr.sin_family = AF_INET;                /* Internet地址族 */
-    addr.sin_port = htons(PORT);              /* 端口号 */
-    addr.sin_addr.s_addr = htonl(INADDR_ANY); /* IP地址 */
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(PORT);
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
 
-    int bindResult = bind(server_socket, (struct sockaddr *)&addr, sizeof(addr)); //连接
+    int bindResult = bind(server_socket, (struct sockaddr *)&addr, sizeof(addr));
 
-    int listenResult = listen(server_socket, 5); //监听
+    int listenResult = listen(server_socket, 5);
     return server_socket;
 }
 
@@ -29,7 +29,7 @@ int wait_client(int server_socket)
     struct sockaddr_in cliaddr;
     int addrlen = sizeof(cliaddr);
     printf("waiting connect .. \n");
-    int client_socket = accept(server_socket, (struct sockaddr *)&cliaddr, &addrlen); //创建一个和客户端交流的套接字
+    int client_socket = accept(server_socket, (struct sockaddr *)&cliaddr, &addrlen);
     printf("accept success %s\n", inet_ntoa(cliaddr.sin_addr));
 
     return client_socket;
@@ -44,25 +44,10 @@ int main()
     char buf[SIZE];
     while (1)
     {
-        int ret = read(client_socket, buf, SIZE - 1);
-        if (ret == -1)
-        {
-            perror("read");
-            break;
-        }
-        if (ret == 0)
-        {
-            break;
-        }
-        buf[ret] = '\0';
-        int i;
-        for (i = 0; i < ret; i++)
-        {
-            buf[i] = buf[i] + 'A' - 'a';
-        }
-
-        printf("%s\n", buf);
-        write(client_socket, buf, ret);
+        int bufSize = read(client_socket, buf, SIZE - 1);
+        buf[bufSize] = '\0';
+        printf("From client: %s\n", buf);
+        write(client_socket, buf, bufSize);
 
         if (strncmp(buf, "end", 3) == 0)
         {
