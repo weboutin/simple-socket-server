6dd3ea7bcdb5f39b28b5b5c18d756cdfc485abed
itgo <634872896@qq.com>
2019-11-10 18:49:40 +0800

    feat: client use shell argv

--- client.c
+++ client.c
@@ -3,23 +3,33 @@
 #include <unistd.h>
 #include <string.h>
 #include <arpa/inet.h>
+#include <stdlib.h>
 
 #define PORT 9990
 #define SIZE 1024
 
-int main()
+int main(int argc, char *argv[])
 {
+    char *hostArgv = argv[1];
+    char *portArgv = argv[2];
+
+    int port = atoi(portArgv);
+
     int client_socket = socket(AF_INET, SOCK_STREAM, 0);
 
     struct sockaddr_in addr;
     addr.sin_family = AF_INET;
-    addr.sin_port = htons(PORT);
+    addr.sin_port = htons(port);
     addr.sin_addr.s_addr = htonl(INADDR_ANY);
-    inet_aton("127.0.0.1", &(addr.sin_addr));
+    inet_aton(hostArgv, &(addr.sin_addr));
 
     int server_socket = connect(client_socket, (struct sockaddr *)&addr, sizeof(addr)); //连接服务器
 
-    printf("connect success\n");
+    printf("Connect result ==> %d\n", server_socket);
+    if (server_socket < 0) {
+        perror("Connect result");
+        exit(1);
+    }
 
     char buf[SIZE] = {0};
 
